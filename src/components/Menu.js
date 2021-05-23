4f89677b8875980ed4279f06b64cd23ffee1dae1
Brendon Henning <brendonhenning@gmail.com>
2021-05-23 15:47:05 -0500

    adjusted nav dot position values

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,16 +13,16 @@ function Menu() {
     nav.style.animationFillMode = open === false ? 'forwards' : 'backwards';
     switch (idx) {
       case 0: 
-        dot.style.top = '190px';
+        dot.style.top = '70px';
         break;
       case 1: 
-        dot.style.top = '285px';
+        dot.style.top = '167px';
         break;
       case 2:
-        dot.style.top = '382px';
+        dot.style.top = '265px';
         break;
       case 3: 
-        dot.style.top = '480px'
+        dot.style.top = '365px'
         break;
       default:
         break;
@@ -34,29 +34,31 @@ function Menu() {
     <div>
       <Hamburger open={open} handleClick={handleClick} /> 
       <div className='menu' id='navbar'>
-        <div id='nav-dot'></div>
-        <ul>
-          <li>
-            <Link activeClass='active' to='home' smooth={true} onClick={() => handleClick(0)}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link activeClass='active' to='work' smooth={true} onClick={() => handleClick(1)}>
-              Work
-            </Link>
-          </li>
-          <li>
-            <Link activeClass='active' to='about' smooth={true} onClick={() => handleClick(2)}>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link activeClass='active' to='contact' smooth={true} onClick={() => handleClick(3)}>
-              Contact
-            </Link>
-          </li>
-        </ul>
+        <div className='position-relative'>
+          <div id='nav-dot'></div>
+          <ul>
+            <li>
+              <Link activeClass='active' to='home' smooth={true} onClick={() => handleClick(0)}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link activeClass='active' to='work' smooth={true} onClick={() => handleClick(1)}>
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link activeClass='active' to='about' smooth={true} onClick={() => handleClick(2)}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link activeClass='active' to='contact' smooth={true} onClick={() => handleClick(3)}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
