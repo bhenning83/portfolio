9e27de5d81ec8fa6d0ac9e8ccc960b4edd59fc8f
Brendon Henning <brendonhenning@gmail.com>
2021-06-09 14:29:53 -0500

    styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,14 +4,14 @@ function Contact() {
   return (
     <div className='section row contact'>
       <div className='col-12 mx-auto contact-info-container d-flex justify-content-center '>
-        <div className='row w-100'>
+        <div className='row w-100 d-flex justify-content-center'>
           <div className='col-5 touch'>
             <h1>Let's</h1>
             <h1>Get</h1>
             <h1>In</h1>
             <h1>Touch</h1>
           </div>
-          <div className='col-2 d-flex justify-content-center'>
+          <div className='col-2 d-flex justify-content-center bar-box'>
             <div className='bar'></div>
           </div>
           <div className='col-5 contact-info-box'>
@@ -26,6 +26,12 @@ function Contact() {
               <p className='email'>@gmail.com</p>
             </a>
           </div>
+        </div>
+      </div>
+      <div className='col-12 d-flex justify-content-center'>
+        <div className='align-self-end end-p-box'>
+          <p>I'm always looking for ways to improve.</p>
+          <p className='pt-2'> If you have a suggestion on how I could do that or would like to discuss a job/project opportunity, I'd love to chat.</p>
         </div>
       </div>
     </div>
